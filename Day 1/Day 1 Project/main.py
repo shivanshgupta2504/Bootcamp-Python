fceba8431f5b9a6a9533fe226a3e95415b416efb
Shivansh Gupta <shivanshgupta2057@gmail.com>
2024-04-18 12:52:03 +0530

    Day 1

---
+++ Day 1/Day 1 Project/main.py
@@ -0,0 +1,10 @@
+# 1. Create a greeting for your program.
+print("Welcome to Band Name Generator")
+# 2. Ask the user for the city that they grew up in.
+city = input("Please enter your city:\n")
+# 3. Ask the user for the name of a pet.
+pet_name = input("Please enter your pet name:\n")
+# 4. Combine the name of their city and pet and show them their band name.
+band_name = city + " " + pet_name
+# 5. Make sure the input cursor shows on a new line:
+print('Your Band Name could be ' + band_name)
---
+++ Day 1/Printing-Input-Variables/main.py
@@ -0,0 +1,17 @@
+# Printing anything on command line or console
+print('Shivansh Gupta')  # or print("Shivansh Gupta")
+print("Hello 'Shivansh'")  # print('Hello "Shivansh"')
+print("Hello \"Shivansh\"")
+
+# Next Line character '\n'
+print("Hello World!\nIt's a nice day")
+
+# Taking Input and variables
+input("What is your name?")  # returns a string from user input
+# To store the input we need variables - placeholders for values
+name = input("What is your name?\n")
+print(name)
+print(len(name))  # prints length of the name variable
+
+# String concatenation
+print("Hello" + " " + name)
