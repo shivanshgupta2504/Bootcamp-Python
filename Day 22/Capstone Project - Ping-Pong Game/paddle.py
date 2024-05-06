ad628f2f467e89a643625993b6a3b983800ea383
Shivansh Gupta <shivanshgupta2057@gmail.com>
2024-05-06 11:25:52 +0530

    Day 22 Capstone Project added

---
+++ Day 22/Capstone Project - Ping-Pong Game/paddle.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(position)
+
+    def go_up(self):
+        self.goto(self.xcor(), self.ycor() + 20)
+
+    def go_down(self):
+        self.goto(self.xcor(), self.ycor() - 20)
+
+    def reset_paddle_position(self, position):
+        self.goto(position)
+    
+    
+    
