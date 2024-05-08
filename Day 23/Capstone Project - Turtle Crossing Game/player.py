3686d4fb7a5d10e8743fa37b89e99fbc107564f5
Shivansh Gupta <shivanshgupta2057@gmail.com>
2024-05-08 12:11:50 +0530

    Day 23 capstone project added

---
+++ Day 23/Capstone Project - Turtle Crossing Game/player.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.go_to_start()
+        self.left(90)
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
+
+    def is_at_finish_line(self):
+        return self.ycor() > FINISH_LINE_Y
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
