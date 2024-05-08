b629795eff76eceefdb8d2168a2ad0a79acc4732
Shivansh Gupta <shivanshgupta2057@gmail.com>
2024-05-08 18:06:26 +0530

    day 20-21 partial changes in highscore

--- Day 20-21/Capstone Project - Snake Game/scoreboard.py
+++ Day 20-21/Capstone Project - Snake Game/scoreboard.py
@@ -8,31 +8,37 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
-        self.update_scoreborard()
+        self.update_scoreboard()
 
-    def update_scoreborard(self):
+    def update_scoreboard(self):
         """
         Update the scoreboard
-        :return:
         """
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         """
         Increase the score
         """
         self.score += 1
-        self.clear()
-        self.update_scoreborard()
+        self.update_scoreboard()
 
-    def game_over(self):
-        """
-        Game over
-        """
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     """
+    #     Game over
+    #     """
+    #     self.goto(0, 0)
+    #     self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
 
