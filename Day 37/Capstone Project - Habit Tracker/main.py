3ce469cc6940e9d6de36b9f4f9e7001a0320f9d1
unknown <shivanshgupta2057@gmail.com>
2024-07-22 21:23:30 +0530

    Day 37 added

---
+++ Day 37/Capstone Project - Habit Tracker/main.py
@@ -0,0 +1,64 @@
+import requests
+from datetime import datetime
+
+USERNAME = "shivanshsg25"
+TOKEN = "vedv4d1vv4sve47v"
+GRAPH_ID = "graph1"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Indoor Cycling",
+    "unit": "Km",
+    "type": "float",
+    "color": "sora",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+post_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
+today = datetime.now()
+
+post_params = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "15",
+}
+
+# response = requests.post(url=post_endpoint, json=post_params, headers=headers)
+# print(response.text)
+
+# Updating a previous post
+day = datetime(year=2024, month=7, day=21).strftime("%Y%m%d")
+
+update_endpoint = f"{post_endpoint}/{day}"
+
+update_params = {
+    "quantity": "10",
+}
+
+# response = requests.put(url=update_endpoint, json=update_params, headers=headers)
+# print(response.text)
+
+# Deleting a post
+delete_endpoint = f"{post_endpoint}/{day}"
+
+response = requests.delete(url=delete_endpoint, headers=headers)
+print(response.text)
